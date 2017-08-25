228b8597ee09fe1aeb72cbcee9e72de221e094f6
Ayman-Mohamed <mahone.alexander52@gmail.com>
2017-08-25 20:41:21 +0200

    first commit

---
+++ app/forgetPassword/index.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ForgetPassword.IndexController', Controller);
+
+    function Controller($window, UserService, FlashService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.reset = reset;
+
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+
+        function reset() {
+            UserService.Reset(vm.user)
+                .then(function () {
+                    FlashService.Success('Password updated');
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });
+        }
+    }
+
+})();
